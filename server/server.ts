0a0dcbaa5cd9428f5b7509eb41d515a456ec0041
TRILX <ruohonen_tero@hotmail.com>
2021-04-16 17:21:31 +0300

    initialiazing failure

--- server/server.ts
+++ server/server.ts
@@ -7,20 +7,20 @@ const PORT: string | number = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+if (process.env.NODE_ENV === 'production') {
+	app.use(express.static(path.join(__dirname, '../client/build')));
+	// 'catching-all' handler to send back React's index.html if a req doesn't match any endpoints above
+	app.use((_req, res) => {
+		res.sendFile(path.join(__dirname, './client/build/index.html'));
+	});
+}
 
-app.use(express.static(path.join(__dirname, '../client/build')));
-// 'catching-all' handler to send back React's index.html if a req doesn't match any endpoints above
-app.use((_req, res) => {
-	res.sendFile(path.join(__dirname, './client/build/index.html'));
-});
-
-
-// if (process.env.NODE_ENV === 'development') {
-// 	app.use(cors({
-// 		origin: 'http://localhost:3000',
-// 		credentials: true,
-// 		methods: 'GET, PUT, POST, PATCH, DELETE'
-// 	}));
-// }
+if (process.env.NODE_ENV === 'development') {
+	app.use(cors({
+		origin: 'http://localhost:3000',
+		credentials: true,
+		methods: 'GET, PUT, POST, PATCH, DELETE'
+	}));
+}
 
 app.listen(PORT, () => console.log(`hosting @${PORT}`));
