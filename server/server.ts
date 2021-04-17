b571660ecf1e5b31be4f7de6b3eb85469c206f46
TRILX <ruohonen_tero@hotmail.com>
2021-04-17 23:02:27 +0300

    move errors

--- server/server.ts
+++ server/server.ts
@@ -1,11 +1,12 @@
 import express from 'express';
 import path from 'path';
 import 'reflect-metadata';
-import { createConnection, Connection } from 'typeorm';
-import { PostgresConnectionOptions } from 'typeorm/driver/postgres/PostgresConnectionOptions';
+// import { createConnection, Connection } from 'typeorm';
+// import { PostgresConnectionOptions } from 'typeorm/driver/postgres/PostgresConnectionOptions';
 import 'dotenv/config';
 import { createRoutes } from './routes/routes';
 import { News } from './models/News';
+const { Client } = require('pg');
 const nodemailer = require('nodemailer');
 const router = express.Router();
 const cors = require('cors')
@@ -35,23 +36,42 @@ if (process.env.NODE_ENV === 'development') {
 
 app.listen(PORT, () => console.log(`hosting @${PORT}`));
 
-const config: PostgresConnectionOptions = {
-	type: 'postgres',
-	host: String(process.env.DB_HOST),
-	port: Number(process.env.DB_PORT),
-	username: String(process.env.DB_USERNAME),
-	password: String(process.env.DB_PASSWORD),
-	database: String(process.env.DB_DATABASE),
-	synchronize: true,
-	logging: false,
-	entities: [
-		News
-	]
-};
 
-createConnection(config).then(async () => {
-	console.log('Server is connected to PostgreSQL database.');
-}).catch(e => console.log(e));
+
+const client = new Client({
+	connectionString: process.env.DATABASE_URL,
+	ssl: {
+		rejectUnauthorized: false
+	}
+});
+
+client.connect();
+
+client.query('SELECT * FROM news;', (err: any, res: { rows: any; }) => {
+	if (err) throw err;
+	for (let row of res.rows) {
+		console.log(JSON.stringify(row));
+	}
+	client.end();
+});
+
+// const config: PostgresConnectionOptions = {
+// 	type: 'postgres',
+// 	host: String(process.env.DB_HOST),
+// 	port: Number(process.env.DB_PORT),
+// 	username: String(process.env.DB_USERNAME),
+// 	password: String(process.env.DB_PASSWORD),
+// 	database: String(process.env.DB_DATABASE),
+// 	synchronize: true,
+// 	logging: false,
+// 	entities: [
+// 		News
+// 	]
+// };
+
+// createConnection(config).then(async () => {
+// 	console.log('Server is connected to PostgreSQL database.');
+// }).catch(e => console.log(e));
 
 // Nodemailer for Contact
 const contactEmail = nodemailer.createTransport({
