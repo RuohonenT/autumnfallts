adfe9b978d82f8ccd0beba7e4a0c704dc5d4aa4c
TRILX <ruohonen_tero@hotmail.com>
2021-04-20 14:26:35 +0300

    add connection to heroku

--- server/server.ts
+++ server/server.ts
@@ -14,6 +14,7 @@ app.use(cors());
 app.use(express.json());
 app.use('/', router);
 app.use('/api', createRoutes());
+app.get('/');
 
 if (process.env.NODE_ENV === 'production') {
 	app.use(express.static(path.join(__dirname, '../client/build')));
