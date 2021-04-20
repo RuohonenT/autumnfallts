a0f03053f81f1ef03b660718c378d5f2188cf166
TRILX <ruohonen_tero@hotmail.com>
2021-04-20 13:15:32 +0300

    add connection to heroku

--- server/server.ts
+++ server/server.ts
@@ -16,7 +16,7 @@ app.use('/', router);
 app.use('/api', createRoutes());
 
 if (process.env.NODE_ENV === 'production') {
-	app.use(express.static(path.join(__dirname, '../client/build')));
+	app.use(express.static(path.join(__dirname, '../app/client/build')));
 	// 'catching-all' handler to send back React's index.html if a req doesn't match any endpoints above
 	app.use((_req, res) => {
 		res.sendFile(path.join(__dirname, '/app/client/build/index.html'));
