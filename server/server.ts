b4e08b1e2fd8d119835c6e08f0ef9dddc7e0875d
TRILX <ruohonen_tero@hotmail.com>
2021-04-27 09:05:59 +0300

    ...

--- server/server.ts
+++ server/server.ts
@@ -33,7 +33,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(express.json());
 app.use('/', router);
-app.use('/api', createRoutes());
+app.use('/news', createRoutes());
 app.get('/');
 app.listen(PORT, () => console.log(`hosting port ${PORT}`));
 
