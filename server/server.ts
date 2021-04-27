34aa3348a8a9844533aebefc734aecc35003174b
TRILX <ruohonen_tero@hotmail.com>
2021-04-27 12:41:48 +0300

    ...

--- server/server.ts
+++ server/server.ts
@@ -68,8 +68,17 @@ app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(express.json());
-app.use('/', router);
+app.use((req, res, next) => {
+	res.header("Access-Control-Allow-Origin", "*");
+	res.header("Access-Control-Allow-Methods", "*");
+	res.header(
+		"Access-Control-Allow-Headers",
+		"Origin, X-Requested-With, Content-Type, Accept"
+	);
+	next();
+});
 app.use('/api', createRoutes);
+app.use('/', router);
 app.get('/');
 app.listen(PORT, () => console.log(`hosting port ${PORT}`));
 
