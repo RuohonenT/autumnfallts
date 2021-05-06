9b39946f98367ae3521c1c377cfa9ec9fcf5f54c
TRILX <ruohonen_tero@hotmail.com>
2021-05-06 21:50:18 +0300

    add edit functions to news

--- client/src/controllers/fetchFunctions.js
+++ client/src/controllers/fetchFunctions.js
@@ -1,7 +1,5 @@
 import axios from "axios";
 
-const URL = process.env.DATABASE_URL;
-
 export const updateNews = async (id, subject, content) => {
 	return await axios.put(`${id}`, { subject, content })
 };
