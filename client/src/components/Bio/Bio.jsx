d9eb8e2b4256b77eeb4ab6f44efc14f1e10bd3f6
TRILX <ruohonen_tero@hotmail.com>
2021-05-27 14:27:17 +0300

    add signup & login functions, still work in progress

--- client/src/components/Bio/Bio.jsx
+++ client/src/components/Bio/Bio.jsx
@@ -1,42 +1,48 @@
 import React, { useState } from 'react';
 import BioEdit from './BioEdit';
+import { useAppContext } from '../../Context';
 import './Bio.css';
 
 const Bio = () => {
     const [bio, setBio] = useState([]);
     const [header, setHeader] = useState([]);
     const [content, setContent] = useState([]);
+    const { token } = useAppContext();
 
 
     return (
         <div className='bio_container'>
             <div className='bio_content'>
-                <>
-                    <BioEdit
-                        bio={bio}
-                        setBio={setBio}
-                        content={content}
-                        setContent={setContent}
-                        header={header}
-                        setHeader={setHeader} />
-                </>
+                {token ?
+                    <>
+                        <BioEdit
+                            bio={bio}
+                            setBio={setBio}
+                            content={content}
+                            setContent={setContent}
+                            header={header}
+                            setHeader={setHeader} />
+                    </>
 
-                <div className='bio_content_innards'>
-                    {bio.length > 0 ?
-                        <>
-                            {
-                                bio.map((bgraph, i) => {
-                                    return (
-                                        <div id={bgraph.header} key={i}>
-                                            <div><h1>{bgraph.header}</h1></div>
-                                            <div><p>{bgraph.content}</p></div>
-                                        </div>
-                                    )
-                                })
-                            }
-                        </>
-                        : <div><p>No Bio Found</p></div>}
-                </div >
+                    :
+
+                    <div className='bio_content_innards'>
+                        {bio.length > 0 ?
+                            <>
+                                {
+                                    bio.map((bgraph, i) => {
+                                        return (
+                                            <div id={bgraph.header} key={i}>
+                                                <div><h1>{bgraph.header}</h1></div>
+                                                <div><p>{bgraph.content}</p></div>
+                                            </div>
+                                        )
+                                    })
+                                }
+                            </>
+                            : <div><p>No Bio Found</p></div>}
+                    </div >
+                }
             </div>
         </div>
     )
